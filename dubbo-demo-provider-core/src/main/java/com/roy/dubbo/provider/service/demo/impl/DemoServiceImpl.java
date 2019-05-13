13a9404279f308c80a3144d6a61a7376d0f1dcfc
roy2010a <roy2010a@163.com>
2019-05-13 18:53:33 +0800

    提交代码20190513,加入了点测试PropertyPlaceholderConfigurer的代码

--- dubbo-demo-provider-core/src/main/java/com/roy/dubbo/provider/service/demo/impl/DemoServiceImpl.java
+++ dubbo-demo-provider-core/src/main/java/com/roy/dubbo/provider/service/demo/impl/DemoServiceImpl.java
@@ -3,6 +3,7 @@ package com.roy.dubbo.provider.service.demo.impl;
 import com.roy.dubbo.exception.HelloWorldException;
 import com.roy.dubbo.provider.service.demo.DemoService;
 import org.springframework.beans.factory.BeanCurrentlyInCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +12,14 @@ import org.springframework.stereotype.Service;
 
 @Service(value = "demoService")
 public class DemoServiceImpl implements DemoService {
+
+    private String name;
+
+    @Value(value = "${name}")
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public Integer add(Integer a, Integer b) {
         System.out.println(String.format("入参为 a = %s, b= %s" , a,b ));
